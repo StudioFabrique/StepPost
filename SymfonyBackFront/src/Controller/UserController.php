c9eccdb60ea938e269d10062cc74b00c2581e7f0
MartinDHLL <martin.dhollande@outlook.fr>
2022-09-13 11:39:08 +0200

    remise sécurité

--- SymfonyBackFront/src/Controller/UserController.php
+++ SymfonyBackFront/src/Controller/UserController.php
@@ -15,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/step/user', name: 'app_')]
-//#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'step_user')]
@@ -25,9 +25,9 @@ class UserController extends AbstractController
         PaginatorInterface $paginator
     ): Response {
 
-        // if (!$this->getUser()) {
-        //     return $this->redirectToRoute('app_login');
-        // }
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
 
         $donner = $userSteps->findAll([], ['id' => 'DESC']);
         $userStep = $paginator->paginate(
