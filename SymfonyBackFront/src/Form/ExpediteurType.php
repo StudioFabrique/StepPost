178cdd3c26a08055e89b48f68e88f0a4c6277779
Martin Dhollande <martin.dhollande@outlook.fr>
2023-01-02 23:39:59 +0100

    correction raison sociale expéditeur

--- SymfonyBackFront/src/Form/ExpediteurType.php
+++ SymfonyBackFront/src/Form/ExpediteurType.php
@@ -126,7 +126,7 @@ class ExpediteurType extends AbstractType
                     'choices' => $this->clientRepository->findActiveClients(),
                     'choice_label' => 'raisonSociale',
                     'mapped' => false,
-                    'required' => $options['type'] == 'create' ? true : false,
+                    'required' => true,
                     'label_attr' => ['class' => 'block text-gray-700 text-sm font-bold mb-2'],
                     'attr' => [
                         'class' => 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4',
