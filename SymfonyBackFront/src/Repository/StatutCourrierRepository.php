c58a229af0b37e7a845099b2b13c6ec47c3df6b8
Martin Dhollande <martin.dhollande@outlook.fr>
2023-01-23 16:21:53 +0100

    correction statistique

--- SymfonyBackFront/src/Repository/StatutCourrierRepository.php
+++ SymfonyBackFront/src/Repository/StatutCourrierRepository.php
@@ -208,7 +208,7 @@ class StatutCourrierRepository extends ServiceEntityRepository
                 f.id
                 '
             )
-            ->join('s.facteur', 'f')
+            ->leftJoin('s.facteur', 'f')
             ->join('s.statut', 'd')
             ->groupBy('s.courrier')
             ->having($facteurId == null ? ("MAX(d.statutCode) = :statutid") : ("MAX(d.statutCode) = :statutid and f.id = :facteurid"))
