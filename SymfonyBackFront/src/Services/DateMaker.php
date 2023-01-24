a265df9abbfeddefe00c653b54d30f347f65a9bf
Martin Dhollande <martin.dhollande@outlook.fr>
2023-01-24 15:36:35 +0100

    datetime zone modifs

--- SymfonyBackFront/src/Services/DateMaker.php
+++ SymfonyBackFront/src/Services/DateMaker.php
@@ -10,14 +10,13 @@ use DateTimeZone;
  */
 class DateMaker
 {
-    public function createFromDateTimeZone($date = 'now', string $zone = "EU"): ?DateTime
+    public function createFromDateTimeZone($date = 'now', string $zone = "UTC"): ?DateTime
     {
         return new DateTime($date, new DateTimeZone($zone));
     }
 
     public function convertDateDefault($date = null): ?DateTime
     {
-        $date = $date != null ? date_create($date) : null;
-        return $date;
+        return $date != null ? $this->createFromDateTimeZone($date, 'EU') : null;
     }
 }
