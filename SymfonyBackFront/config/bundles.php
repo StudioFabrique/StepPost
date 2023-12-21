6d5a9c7e6b8e28acbf7b85473c4e250b9424a627
Martin Dhollande <martindhollande@Air-de-Martin.lan>
2023-12-21 14:42:14 +0100

    sécurisation + ajout timezone default

--- SymfonyBackFront/config/bundles.php
+++ SymfonyBackFront/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
