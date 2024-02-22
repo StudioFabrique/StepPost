1de37de99b23f977191cb82af063732a67ff98e2
Martin Dhollande <martindhollande@MacBook-Air-de-Martin.local>
2024-02-22 13:30:35 +0100

    démonstration

--- SymfonyBackFront/src/Controller/UserController.php
+++ SymfonyBackFront/src/Controller/UserController.php
@@ -17,7 +17,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-/* 
+/*
 Cette classe donne la possiblité de créer, modifier, activer et supprimer un admin.
 Seul le super admin a les droits d'accès aux différentes méthodes de cette classe.
 */
@@ -35,7 +35,7 @@ class UserController extends AbstractController
         $this->messageService = $messageService;
     }
 
-    /* 
+    /*
     Retourne un template twig avec la liste des admins avec une pagination.
     */
     #[Route('/', name: 'admin')]
@@ -55,7 +55,7 @@ class UserController extends AbstractController
         return $this->render('admin/index.html.twig', $this->requestManager->GenerateRenderRequest('admin', $request, $dataPagination, $data));
     }
 
-    /* 
+    /*
         La méthode new permet de créer un administrateur ayant comme rôle ROLE_ADMIN
     */
     #[Route('/ajouter', name: 'admin_add')]
@@ -80,7 +80,7 @@ class UserController extends AbstractController
         return $this->renderForm('admin/new.html.twig', $this->requestManager->GenerateRenderFormRequest('admin', $request, $form));
     }
 
-    /* 
+    /*
         La méthode edit permet de modifier les informations d'un administrateur
     */
     #[Route('/edit/{id}', name: 'admin_edit')]
@@ -108,7 +108,7 @@ class UserController extends AbstractController
         return $this->renderForm('admin/edit.html.twig', $this->requestManager->GenerateRenderFormRequest('admin', $request, $form));
     }
 
-    /* 
+    /*
         La méthode editPassword permet de modifier le mot de passe d'un administrateur
     */
     #[Route(name: 'edit_password', path: '/editPassword')]
@@ -136,7 +136,7 @@ class UserController extends AbstractController
         return $this->renderForm('admin/edit.html.twig', $this->requestManager->GenerateRenderFormRequest('admin', $request, $form));
     }
 
-    /* 
+    /*
         La méthode delete permet de supprimer un administrateur
     */
     #[Route('/delete/{id}', name: 'admin_delete')]
